453085b94335f51f56fe9cf96fd4a19f3f389d46
MWandall <mwandall99@gmail.com>
2025-09-22 08:44:55 -0500

    comments to proj0

--- proj0.c
+++ proj0.c
@@ -1,10 +1,15 @@
 #include "types.h"
 #include "user.h"
 
-// ADDED new 
+// ADDED new command for user to call 'proj0 <some user string>'. 
+// will return 'CSE3320 proj0 printing in user space: <some user string>'
+// also see makefile UPROGS:175 for adding proj0 to the 'ls' call menu. (line number may change with future development)
+ 
 int
 main(int argc, char *argv[])
 {
+
+    //NOTE: the agr of '1' is for stdout
 
   printf(1, "CSE3320 proj0 printing in user space: ");
 
